b6ea18f9e87014649b1cf14c73fd6deb5a101f0f
GabrielFranciscoSM <gabrielfranciscosm@gmail.com>
2025-11-20 19:27:15 +0100

    closes #28 feat: Representación dija de secciones
    Según la HU1 #3, las secciones son Pescadería, Panadería, Frutería, Charcutería y Carnicería, así que represento estos símbolos en un array de símbolos.
    
    Para asegurar que no se cambien las secciones disponibles, uso .freeze() para que no se altere el array.
    
    Para seguir con las buenas prácticas de ruby uso %i para representar los símbolos sin ":".

--- lib/genrador_horario/generador_horario.rb
+++ lib/genrador_horario/generador_horario.rb
@@ -1,4 +1,5 @@
 module GeneradorHorarios
   DIAS_DE_LA_SEMANA = %i[lunes martes miércoles jueves viernes sábado].freeze
   TURNOS = %i[mañana tarde].freeze
+  SECCIONES = %i[pescadería panadería fruta charcutería carnicería].freeze
 end
