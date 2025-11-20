51b8dfdfb204f778fa547ceb016bdde22255effb
GabrielFranciscoSM <gabrielfranciscosm@gmail.com>
2025-11-20 19:58:45 +0100

    closes #23 Se almacena los últimos tres turnos del trabajador
    Para poder verificar el requisito de rotación de turnos  es necesario almacenar los últimos tres turnos de cada trabajador.
    
    Aunque el último turno cambia cada semana, el cambio debe de manejarse con delicadeza siguiendo el requisito de rotación de turnos #18. Por lo tanto, se mantiene como atributo de sólo lectura.

--- lib/genrador_horario/domino/trabajador.rb
+++ lib/genrador_horario/domino/trabajador.rb
@@ -1,7 +1,7 @@
 module GeneradorHorarios
   module Dominio
     class Trabajador
-      attr_reader :nombre_y_apellidos, :último_día_libre
+      attr_reader :nombre_y_apellidos, :último_día_libre, :últimos_tres_turnos
     end
   end
 end
