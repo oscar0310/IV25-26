8a48fddbef2dbe857c9a5978cad581c70f005e62
GabrielFranciscoSM <gabrielfranciscosm@gmail.com>
2025-11-20 19:52:27 +0100

    closes #22 Se almacena el último día libre
    Para poder calcular el próximo día libre de cada trabajador alamcenaremos su último día libre
    
    Aunque el último día libre cambia cada semana, el cambio debe de manejarse con delicadeza siguiendo el requisito de rotación de días #17. Por lo tanto, se mantiene como atributo de sólo lectura.

--- lib/genrador_horario/domino/trabajador.rb
+++ lib/genrador_horario/domino/trabajador.rb
@@ -1,7 +1,7 @@
 module GeneradorHorarios
   module Dominio
     class Trabajador
-      attr_reader :nombre_y_apellidos
+      attr_reader :nombre_y_apellidos, :último_día_libre
     end
   end
 end
