6e548e120e3a92cac318789fa2f0de4a60b336c2
Óscar Fernández Rodríguez <e.oscar12@go.ugr.es>
2026-01-15 00:37:49 +0100

    fix: #45 definiendo la función comprobar_nTrabajador
    Se define esta función para comprobar que el nombre de un trabajador sigue el formato correcto, siendo así un trabajador válido

--- lib/generador_horario/obt_datos.rb
+++ lib/generador_horario/obt_datos.rb
@@ -5,4 +5,14 @@ module GeneradorHorario
             raise ArgumentError, "El archivo de con la información de los trabajadores debe ser .csv (se recibió: #{archivo})."
         end
     end  
+    
+    def comprobar_nTrabajador(nombre)
+        formato1=/^[a-zA-ZáéíóúñÑ]+\s+[a-zA-ZáéíóúñÑ]+$/
+        formato2=/^[a-zA-ZáéíóúñÑ]+\s+[a-zA-ZáéíóúñÑ]+\s+[a-zA-ZáéíóúñÑ]+$/
+
+        unless nombre=~formato1 || nombre=~formato2
+            raise ArgumentError, "El nombre: #{nombre} pasado en el archivo de entrada no es un nombre valido, tiene que seguir el formato nombre apellido o nombre apellido1 apellido2"
+        end
+    end
+
 end
