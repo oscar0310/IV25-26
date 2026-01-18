c4a939ac80ad78b0edd929fc61ef6153b578ea74
Óscar Fernández Rodríguez <e.oscar12@go.ugr.es>
2026-01-18 12:01:32 +0100

    fix: #32 Añadimos un reporter personalizado para minitest
    Las salidas de minitest son muy básicas y no son muy autoexplicativas, es por esto que creo un ReporterPersonalizado que es el encargado de modelar la salida en el se crean dos métodos. start que es el encargado de mostrar la primera linea al iniciar los test y record que se encarga de modelar el nombre del test ya que usaré nombres autoexplicativos y los deja como una sentencia limpia. Por ejemplo si el test se llama test_validacion_de_archivo_entrada_csv, se dejará como nombre en la salida de los test: validacion archivo de entrada csv. Después comprueba si lo ha pasado o no y en cada caso se añade un emoji correspondiente para que sea más visual. En caso de no haberlo pasado se muestra el error junto al test incorrecto

--- test/test.rb
+++ test/test.rb
@@ -1,6 +1,22 @@
 require 'minitest/autorun'
 
 
+class ReporterPersonalizado<Minitest::AbstractReporter
+  def start
+    puts "\n--- Inicio de Tests ---\n\n"
+  end
+
+  def record(result)
+    nombre = result.name.gsub('test_', '').gsub('_', ' ')
+    
+    if result.passed?
+      puts "  ✔️  #{nombre}"
+    else
+      puts "  ❌  #{nombre}"
+      puts "     └─ Error: #{result.failures.first.message}"
+    end
+  end
+end
 
 
 
